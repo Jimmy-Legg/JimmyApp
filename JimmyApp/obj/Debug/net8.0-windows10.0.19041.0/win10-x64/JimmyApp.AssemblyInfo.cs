ef3d6145d9bc5a2708303ddd56e1f0cb5c03c1a9
Jimmy-legg <jimmy.legg18@gmail.com>
2024-03-20 21:46:35 +0100

    fixing project

--- JimmyApp/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/JimmyApp.AssemblyInfo.cs
+++ JimmyApp/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/JimmyApp.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JimmyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+44a5aa474908f2e0f73f1211aeb2aa2c05699bd5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+582a2855875e436cbf9b4132648c3f87aafe4dc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("JimmyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JimmyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
