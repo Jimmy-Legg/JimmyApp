582a2855875e436cbf9b4132648c3f87aafe4dc8
Jimmy-legg <jimmy.legg18@gmail.com>
2024-03-20 16:25:36 +0100

    changing image

--- JimmyApp/obj/Debug/net8.0-ios/iossimulator-x64/JimmyApp.AssemblyInfo.cs
+++ JimmyApp/obj/Debug/net8.0-ios/iossimulator-x64/JimmyApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JimmyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ddf4472f4af31d78052ac3ba61ab99aa7c3f2481")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+44a5aa474908f2e0f73f1211aeb2aa2c05699bd5")]
 [assembly: System.Reflection.AssemblyProductAttribute("JimmyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JimmyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
